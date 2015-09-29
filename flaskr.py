776b5dad71b96ccf7f900e6317af6e17b91f0620
Tiancheng Wu <tiancheng.wu@cerner.com>
2015-09-29 10:04:09 -0500

    edit & delete entry function added, Flask-paginate

--- flaskr.py
+++ flaskr.py
@@ -3,6 +3,7 @@ import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 from contextlib import closing
 from datetime import datetime
+from flask.ext.paginate import Pagination
 
 #configuration
 DATABASE = '/tmp/flaskr.db'
@@ -36,16 +37,28 @@ def teardown_request(exception):
 
 @app.route('/')
 def show_entries():
+    search = False
+    q = request.args.get('q')
+    if q:
+        search = True
+    try:
+        page = int(request.args.get('page', 1))
+    except ValueError:
+        page = 1
     cur = g.db.execute('select title, text, time, id from entries order by id desc')
     entries = [dict(title=row[0], text=row[1], time = row[2][:-10], entryId = row[3]) for row in cur.fetchall()]
-    return render_template('show_entries.html', entries=entries)
+    pagination = Pagination(page=page, total=len(entries), search=search, record_name='entries')
+    return render_template('show_entries.html', entries=entries, pagination=pagination,)
 
 @app.route('/entry/<entryId>')
 def show_entry(entryId):
-	cur = g.db.execute('select title, text, time from entries where id = ?', [entryId])
-	row = cur.fetchall()
-	entry = dict(title = row[0][0], text = row[0][1], time= row[0][2][:-10])
-	return render_template('show_entry.html', entry = entry)
+    cur = g.db.execute('select title, text, time from entries where id = ?', [entryId])
+    row = cur.fetchall()
+    if row:
+        entry = dict(title = row[0][0], text = row[0][1], time= row[0][2][:-10] ,id = entryId) 
+        return render_template('show_entry.html', entry = entry)
+    else:
+        return show_entries()
 
 @app.route('/add', methods=['POST'])
 def add_entry():
@@ -55,6 +68,25 @@ def add_entry():
                  [request.form['title'], request.form['text'], datetime.now()])
     g.db.commit()
     flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+@app.route('/edit/<entryId>', methods=['POST'])
+def edit_entry(entryId):
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('update entries set title=?, text=?, time=? where id = ?',
+                 [request.form['title'], request.form['text'], datetime.now(), entryId])
+    g.db.commit()
+    flash('Entry was successfully updated')
+    return redirect(url_for('show_entries'))
+
+@app.route('/delete/<entryId>')
+def delete_entry(entryId):
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('delete from entries where id = ?', [entryId])
+    g.db.commit()
+    flash('Entry was successfully deleted')
     return redirect(url_for('show_entries'))
 
 @app.route('/login', methods=['GET', 'POST'])
