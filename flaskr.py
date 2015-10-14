34e30e6e0bdbc69f71d2210c5ba1b66fcbef8c1e
Tiancheng Wu <tiancheng.wu@cerner.com>
2015-10-14 14:22:33 -0500

    ui change, stop clearing db

--- flaskr.py
+++ flaskr.py
@@ -1,10 +1,11 @@
 #all the imports
 import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, jsonify, render_template_string
 from contextlib import closing
 from datetime import datetime
 from flask.ext.paginate import Pagination
 from flask.ext.misaka import Misaka
+from flask.ext.triangle import Triangle
 
 #configuration
 DATABASE = '/tmp/flaskr.db'
@@ -17,15 +18,10 @@ PASSWORD = '12345'
 app = Flask(__name__)
 app.config.from_object(__name__)
 Misaka(app)
+Triangle(app)
 
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
-
-def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
 
 @app.before_request
 def before_request():
