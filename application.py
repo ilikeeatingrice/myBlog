f422e4058b84847bfb058653ee02906c83c608cc
Tiancheng Wu <tiancheng.wu@cerner.com>
2015-10-16 08:01:38 -0500

    test

--- application.py
+++ application.py
@@ -10,7 +10,7 @@ from flask.ext.triangle import Triangle
 #configuration
 DATABASE = 'tiancheng.db'
 DEBUG = True
-SECRET_KEY = "eat rice"
+SECRET_KEY = "eatrice"
 USERNAME = 'don.jobs'
 PASSWORD = '21654321'
 
